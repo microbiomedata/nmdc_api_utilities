d9b2b746c54372cb0bf71cef16fa176c3581ea87
olivia.hess <olivia.hess@pnnl.gov>
2025-03-12 09:53:30 -0400

    doc update

--- nmdc_api_utilities/nmdc_search.py
+++ nmdc_api_utilities/nmdc_search.py
@@ -5,9 +5,11 @@ logger = logging.getLogger(__name__)
 
 
 class NMDCSearch:
+    """
+        Base class for interacting with the NMDC API. Sets the base URL for the API based on the environment.
+    """
     def __init__(self, env="prod"):
         """
-        Base class for interacting with the NMDC API. Sets the base URL for the API based on the environment.
         Environment is defaulted to the production isntance of the API. This functionality is in place for monthly testing of the runtime updates to the API.
         params 
             env: str
