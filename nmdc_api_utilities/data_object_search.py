70cb0717a973bd3aa51c508bf6d8dd7c89c25314
olivia.hess <olivia.hess@pnnl.gov>
2025-03-19 15:08:03 -0400

    get data objects by study id

--- nmdc_api_utilities/data_object_search.py
+++ nmdc_api_utilities/data_object_search.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from nmdc_api_utilities.collection_search import CollectionSearch
 import logging
-
+import requests
+import urllib.parse
 logger = logging.getLogger(__name__)
 
 
@@ -12,3 +13,25 @@ class DataObjectSearch(CollectionSearch):
 
     def __init__(self, env="prod"):
         super().__init__(collection_name="data_object_set", env=env)
+
+    def get_data_objects_for_studies(self, study_id: str):
+        """
+        Get data objects by study id.
+        """
+        url = f"{self.base_url}/data_objects/study/{study_id}"
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            logger.error("API request failed", exc_info=True)
+            raise RuntimeError("Failed to get data_objects from NMDC API") from e
+        else:
+            logging.debug(
+                f"API request response: {response.json()}\n API Status Code: {response.status_code}"
+            )
+
+        results = response.json()
+
+        return results
+    
+    
