13f9a434247e05eb6bbb5c8c4e29d113e33ab087
olivia.hess <olivia.hess@pnnl.gov>
2025-11-12 10:47:37 -0500

    testing githubbot

--- nmdc_api_utilities/collection_search.py
+++ nmdc_api_utilities/collection_search.py
@@ -207,9 +207,7 @@ class CollectionSearch(NMDCSearch):
     def get_record_by_id(
         self,
         collection_id: str,
-        max_page_size: int = 100,
-        fields: str = "",
-    ) -> list[dict]:
+        max_page_size: int = 100,fields: str = "",) -> list[dict]:
         """
         Get a collection of data from the NMDC API by id.
 
@@ -242,9 +240,7 @@ class CollectionSearch(NMDCSearch):
             logger.error("API request failed", exc_info=True)
             raise RuntimeError("Failed to get collection by id from NMDC API") from e
         else:
-            logging.debug(
-                f"API request response: {response.json()}\n API Status Code: {response.status_code}"
-            )
+            logging.debug(f"API request response: {response.json()}\n API Status Code: {response.status_code}")
         results = response.json()
         return results
 
