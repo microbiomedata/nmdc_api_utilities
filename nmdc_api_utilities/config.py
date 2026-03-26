32e17615f9766ba77b9ca1b52ad1e4d1fed3ca86
eecavanna <eecavanna@users.noreply.github.com>
2026-03-26 00:49:25 -0700

    Clarify variable name

--- nmdc_api_utilities/config.py
+++ nmdc_api_utilities/config.py
@@ -11,7 +11,7 @@ import re
 # variable, falling back to the URL of the production instance of the NMDC Runtime API if the
 # environment variable is not defined. Also validate that the value resembles a URL. The end
 # result is that this module will export a validated `API_BASE_URL`.
-DEFAULT_API_BASE_URL = "https://api.microbiomedata.org"
-API_BASE_URL = os.getenv("API_BASE_URL", DEFAULT_API_BASE_URL)
+PRODUCTION_API_BASE_URL = "https://api.microbiomedata.org"
+API_BASE_URL = os.getenv("API_BASE_URL", PRODUCTION_API_BASE_URL)
 if not isinstance(API_BASE_URL, str) or not re.match(r"^https?://", API_BASE_URL):
     raise ValueError(f"Invalid API_BASE_URL: {API_BASE_URL}")
