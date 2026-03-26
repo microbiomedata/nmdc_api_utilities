5d05ff86d79d3f4613104b713f879b18e5731d5c
eecavanna <eecavanna@users.noreply.github.com>
2026-03-26 12:14:38 -0700

    Restore support for deprecated ENV environment variable

--- nmdc_api_utilities/config.py
+++ nmdc_api_utilities/config.py
@@ -7,14 +7,33 @@ import os
 import re
 
 
-# Populate the `API_BASE_URL` configuration variable based upon the `API_BASE_URL` environment
-# variable, falling back to the URL of the production instance of the NMDC Runtime API if the
-# environment variable is not defined. Also validate that the value resembles a URL. The end
-# result is that this module will export a validated `API_BASE_URL` with trailing slashes removed.
+# Base URL of the production instance of the NMDC Runtime API.
 PRODUCTION_API_BASE_URL = "https://api.microbiomedata.org"
+
+# (Deprecated) Base URL of the internal development instance of the NMDC Runtime API.
+# 
+# Caution: This instance undergoes spontaneous changes throughout each release cycle
+#          and NMDC development team members discourage its use by anyone not on
+#          the NMDC development team. It is here for backwards compatibility with
+#          previous versions of this Python package.
+#
+_DEVELOPMENT_API_BASE_URL = "https://api-dev.microbiomedata.org"
+
+# (Deprecated) Consider the value of the `ENV` environment variable, whose valid values
+#              are "prod" and "dev".
+env_raw = os.getenv("ENV", default="prod")
+
+# Derive an `api_base_url` value from the `API_BASE_URL` environment variable.
+# Note: The `API_BASE_URL` is the successor to the deprecated `ENV` environment variable.
 api_base_url_raw = os.getenv("API_BASE_URL", default=PRODUCTION_API_BASE_URL)
-if not isinstance(api_base_url_raw, str) or not re.match(
-    r"^https?://", api_base_url_raw
-):
+if not isinstance(api_base_url_raw, str) or not re.match(r"^https?://", api_base_url_raw):
     raise ValueError(f"Invalid API_BASE_URL: {api_base_url_raw}")
-API_BASE_URL = api_base_url_raw.rstrip("/")
+api_base_url = api_base_url_raw.rstrip("/")
+
+# The `API_BASE_URL` environment variable allows users to specify any URL for the
+# NMDC Runtime API, whereas the `ENV` environment variable only allows users to select
+# between the production and development instances. The logic below prioritizes the `ENV`
+# environment variable for backwards compatibility.
+API_BASE_URL = PRODUCTION_API_BASE_URL if env_raw == "prod" else \
+    _DEVELOPMENT_API_BASE_URL if env_raw == "dev" else \
+    api_base_url
