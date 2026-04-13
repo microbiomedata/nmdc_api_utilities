a7f4ee795af1365f062d60db7a1a2a9c3aeb6722
Katherine Heal <katherine.heal@pnnl.gov>
2026-04-13 12:19:42 -0700

    Improve lat_long_filters docstrings

--- nmdc_api_utilities/lat_long_filters.py
+++ nmdc_api_utilities/lat_long_filters.py
@@ -7,32 +7,27 @@ logger = logging.getLogger(__name__)
 
 class LatLongFilters(ABC):
     """
-    Mixin class with methods to interact with the runtime NMDC API collections that
-    can be searched by latitude and longitude.
+    Mixin class with methods to interact with collections that
+    can be searched by latitude and longitude via the NMDC API.
     """
 
     def get_record_by_latitude(
         self, comparison: str, latitude: float, page_size=25, fields="", all_pages=False
     ):
         """
-        Get a record from the NMDC API by latitude comparison.
+        Retrieve records by latitude filter via the NMDC API.
 
         Parameters
         ----------
         comparison: str
-            The comparison to use to query the record. MUST BE ONE OF THE FOLLOWING:
-                eq    - Matches values that are equal to the given value.
-                gt    - Matches if values are greater than the given value.
-                lt    - Matches if values are less than the given value.
-                gte    - Matches if values are greater or equal to the given value.
-                lte - Matches if values are less or equal to the given value.
+            The comparison to use to query the record. See Notes for more details.
         latitude: float
             The latitude of the record to query.
         page_size: int
             The number of results to return per page. Default is 25.
         fields: str
             The fields to return. Default is all fields.
-            Example: "id,name,description,alternative_identifiers,file_size_bytes,md5_checksum,data_object_type,url,type"
+            Example: "id,name,description,type"
         all_pages: bool
             True to return all pages. False to return the first page. Default is False.
 
@@ -45,6 +40,16 @@ class LatLongFilters(ABC):
         ------
         ValueError
             If the comparison is not one of the allowed comparisons.
+
+        Notes
+        -----
+        The ``comparison`` must be one of the following: "eq", "gt", "lt", "gte", "lte".
+
+        - eq : Matches values that are equal to the given value.
+        - gt : Matches if values are greater than the given value.
+        - lt : Matches if values are less than the given value.
+        - gte : Matches if values are greater or equal to the given value.
+        - lte : Matches if values are less or equal to the given value.
 
         """
         allowed_comparisons = ["eq", "gt", "lt", "gte", "lte"]
@@ -69,24 +74,19 @@ class LatLongFilters(ABC):
         all_pages: bool = False,
     ) -> list[dict]:
         """
-        Get a record from the NMDC API by longitude comparison.
+        Retrieve records by longitude filter via the NMDC API.
 
         Parameters
         ----------
         comparison: str
-            The comparison to use to query the record. MUST BE ONE OF THE FOLLOWING:
-                eq    - Matches values that are equal to the given value.
-                gt    - Matches if values are greater than the given value.
-                lt    - Matches if values are less than the given value.
-                gte    - Matches if values are greater or equal to the given value.
-                lte - Matches if values are less or equal to the given value.
+            The comparison to use to query the record. See Notes for more details.
         longitude: float
             The longitude of the record to query.
         page_size: int
             The number of results to return per page. Default is 25.
         fields: str
             The fields to return. Default is all fields.
-            Example: "id,name,description,alternative_identifiers,file_size_bytes,md5_checksum,data_object_type,url,type"
+            Example: "id,name,description,type"
         all_pages: bool
             True to return all pages. False to return the first page. Default is False.
 
@@ -100,6 +100,15 @@ class LatLongFilters(ABC):
         ValueError
             If the comparison is not one of the allowed comparisons.
 
+        Notes
+        -----
+        The ``comparison`` must be one of the following: "eq", "gt", "lt", "gte", "lte".
+
+        - eq : Matches values that are equal to the given value.
+        - gt : Matches if values are greater than the given value.
+        - lt : Matches if values are less than the given value.
+        - gte : Matches if values are greater or equal to the given value.
+        - lte : Matches if values are less or equal to the given value.
         """
         allowed_comparisons = ["eq", "gt", "lt", "gte", "lte"]
         if comparison not in allowed_comparisons:
@@ -124,24 +133,14 @@ class LatLongFilters(ABC):
         all_pages: bool = False,
     ) -> list[dict]:
         """
-        Get a record from the NMDC API by latitude and longitude comparison.
+        Retrieve records by latitude and longitude filters via the NMDC API.
 
         Parameters
         ----------
         lat_comparison: str
-            The comparison to use to query the record for latitude. MUST BE ONE OF THE FOLLOWING:
-                eq    - Matches values that are equal to the given value.
-                gt    - Matches if values are greater than the given value.
-                lt    - Matches if values are less than the given value.
-                gte    - Matches if values are greater or equal to the given value.
-                lte - Matches if values are less or equal to the given value.
+            The comparison to use to query the record for latitude. See Notes for more details.
         long_comparison: str
-            The comparison to use to query the record for longitude. MUST BE ONE OF THE FOLLOWING:
-                eq    - Matches values that are equal to the given value.
-                gt    - Matches if values are greater than the given value.
-                lt    - Matches if values are less than the given value.
-                gte    - Matches if values are greater or equal to the given value.
-                lte - Matches if values are less or equal to the given value.
+            The comparison to use to query the record for longitude. See Notes for more details.
         latitude: float
             The latitude of the record to query.
         longitude: float
@@ -150,7 +149,7 @@ class LatLongFilters(ABC):
             The number of results to return per page. Default is 25.
         fields: str
             The fields to return. Default is all fields.
-            Example: "id,name,description,alternative_identifiers,file_size_bytes,md5_checksum,data_object_type,url,type"
+            Example: "id,name,description,type"
         all_pages: bool
             True to return all pages. False to return the first page. Default is False.
 
@@ -163,6 +162,16 @@ class LatLongFilters(ABC):
         ------
         ValueError
             If the comparison is not one of the allowed comparisons.
+
+        Notes
+        -----
+        ``lat_comparison`` and ``long_comparison`` must be one of the following:
+
+        - eq : Matches values that are equal to the given value.
+        - gt : Matches if values are greater than the given value.
+        - lt : Matches if values are less than the given value.
+        - gte : Matches if values are greater or equal to the given value.
+        - lte : Matches if values are less or equal to the given value.
 
         """
         allowed_comparisons = ["eq", "gt", "lt", "gte", "lte"]
