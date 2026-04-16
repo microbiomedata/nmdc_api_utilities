acc23371911fd90501e9ea86e0c1dfd2a8f287b8
Sujay Patil <sujaysanjeev.patil@gmail.com>
2026-04-16 12:56:58 -0700

    remove ProtocolExecutionSearch and add NMDCSearch to package level re-exports

--- nmdc_api_utilities/test/test_linked_instances.py
+++ nmdc_api_utilities/test/test_linked_instances.py
@@ -2,8 +2,8 @@
 import logging
 from itertools import chain
 
+from nmdc_api_utilities import NMDCSearch
 from nmdc_api_utilities.config import API_BASE_URL
-from nmdc_api_utilities.nmdc_search import NMDCSearch
 
 logging.basicConfig(level=logging.DEBUG)
 
